1498a316b0e5f532935e1b287d606bfc51357fb2
Banana-Master <tjdgh0313@gmail.com>
2023-09-20 14:45:55 +0900

    ✅ 회원가입페이지 닉네임 지역, 정당 추가 서버연결 필요함

---
+++ src/data/data.js
@@ -0,0 +1,92 @@
+export const partys = [
+  {
+    id: 1,
+    party: "숭구리당당",
+    color: "#FFC436",
+  },
+  {
+    id: 2,
+    party: "헌누리당",
+    color: "red",
+  },
+  {
+    id: 3,
+    party: "각설당",
+    color: "#337CCF",
+  },
+];
+
+export const locations = [
+  {
+    id: 1,
+    location: "서울",
+  },
+  {
+    id: 2,
+    location: "경기",
+  },
+  {
+    id: 3,
+    location: "인천",
+  },
+  {
+    id: 4,
+    location: "부산",
+  },
+  {
+    id: 5,
+    location: "대구",
+  },
+  {
+    id: 6,
+    location: "광주",
+  },
+  {
+    id: 7,
+    location: "대전",
+  },
+  {
+    id: 8,
+    location: "울산",
+  },
+  {
+    id: 9,
+    location: "세종",
+  },
+  {
+    id: 10,
+    location: "강원",
+  },
+  {
+    id: 11,
+    location: "경남",
+  },
+  {
+    id: 12,
+    location: "경북",
+  },
+  {
+    id: 13,
+    location: "전남",
+  },
+  {
+    id: 14,
+    location: "전북",
+  },
+  {
+    id: 15,
+    location: "충남",
+  },
+  {
+    id: 16,
+    location: "충북",
+  },
+  {
+    id: 17,
+    location: "제주",
+  },
+  {
+    id: 18,
+    location: "기타",
+  },
+];
