79810c336a68498e87053efa75250751506646ea
pmaker126 <pmaker126@gmail.com>
2023-09-20 13:33:15 +0900

    ✅ /login 회원가입 이동 추가

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { getTokenFromCookie, setCookie } from "../auth/cookie";
 
 function Login() {
@@ -84,13 +84,17 @@ function Login() {
               </div>
             </div>
 
-            <div className="flex justify-around">
+            <div className="flex flex-col">
               <button
                 type="submit"
                 className="h-12 pl-3 w-full flex mt-4 justify-center rounded-md bg-gray-400 px-3 py-2 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-500"
               >
                 로그인
               </button>
+            </div>
+            <div className="flex flex-row justify-center">
+              <p className="text-lg mt-2">아직 회원이 아니신가요?</p>
+              <Link to="/join" className="text-lg ml-3 mt-2 font-bold">회원가입</Link>
             </div>
           </form>
         </div>
