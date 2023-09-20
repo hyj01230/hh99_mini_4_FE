c47c54aba2e7d2adb74701ebd5a36357327923a7
Banana-Master <tjdgh0313@gmail.com>
2023-09-20 16:33:46 +0900

    ✅ 네비게이션 수정

--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -136,7 +136,7 @@ function Join() {
     <>
       <div className="flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-[url('http://www.kasancamera.co.kr/shop/data/goods/2013110813095011.jpg')] bg-bottom bg-cover">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 className="mt-5 text-center text-3xl font-bold leading-9 tracking-tight text-gray-900">
+          <h2 className="mt-5 text-center text-3xl font-bold leading-9 tracking-tight text-gray-900" onClick={() => {navigate('/')}}>
             Town Assembly
           </h2>
         </div>
